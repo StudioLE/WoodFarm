9230ea7c0fcb0f928dc3527f292c5e283134368e
Laurence Elsdon <git@elsdon.me>
2020-03-27 14:36:45 +0000

    Moved hugo files

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const assets_npm = function() {
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js'
   ])
-  .pipe(dest('hugo/resources/assets/vendor'))
+  .pipe(dest('resources/assets/vendor'))
 }
 
 // Copy bootstrap less
@@ -24,7 +24,7 @@ const assets_bootstrap = function() {
     'node_modules/bootstrap/less/variables.less',
     'node_modules/bootstrap/less/variables.less',
     ])
-  .pipe(dest('hugo/static/assets/bootstrap/less'))
+  .pipe(dest('static/assets/bootstrap/less'))
 }
 
 // Copy bootstrap less mixins
@@ -32,24 +32,24 @@ const assets_mixins = function() {
   return src([
     'node_modules/bootstrap/less/mixins/*'
     ])
-  .pipe(dest('hugo/static/assets/bootstrap/less/mixins'))
+  .pipe(dest('static/assets/bootstrap/less/mixins'))
 }
 
 // Copy font awesome assets
 const assets_font_awesome = function() {
   return src('node_modules/font-awesome/fonts/*')
-  .pipe(dest('hugo/static/assets/fonts'))
+  .pipe(dest('static/assets/fonts'))
 }
 
 // Build app CSS
 const css = function() {
-  return src('hugo/resources/src/css/style.less')
+  return src('resources/src/css/style.less')
   .pipe(gp_less({ paths: [
-    'hugo/static/'
+    'static/'
   ] }))
   .pipe(gp_rename('app.css'))
   .pipe(gp_clean_css())
-  .pipe(dest('hugo/resources/assets/css'))
+  .pipe(dest('resources/assets/css'))
 }
 
 exports.build = series(assets_npm, assets_bootstrap, assets_mixins, assets_font_awesome, css)
